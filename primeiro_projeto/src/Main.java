ea367e3d50adc6286f8e4063e269fbc49016c814
Lucas <superlucas2005@gmail.com>
2026-02-07 09:25:21 -0300

    first commit

---
+++ primeiro_projeto/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Primeiro misturar os ingredientes  ");
+        System.out.println("Depois bater a massa ");
+        System.out.println("Depois assar o bolo ");
+
+    }
+}
