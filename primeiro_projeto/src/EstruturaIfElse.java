aab66ecdb1a04f055301b0c33916d8754d901ba5
Lucas <superlucas2005@gmail.com>
2026-02-09 17:21:36 -0300

    If-else

---
+++ primeiro_projeto/src/EstruturaIfElse.java
@@ -0,0 +1,31 @@
+public class EstruturaIfElse {
+    public static void main(String[] args) {
+//        int idade = 30;
+////
+////        if(idade >= 18){
+////            System.out.println("voce eh maior de idade");
+////        } else{
+////            System.out.println("voce eh menor de idade");
+////        }
+
+//        boolean estaChovendo = true;
+//
+//        if(!estaChovendo) {
+//            System.out.println("Vamos sair para passear ");
+//        } else{
+//            System.out.println("Vamos ficar em casa ");
+//        }
+
+          boolean temDinheiro = true;
+          boolean temCartao = true;
+
+          if(temDinheiro && temCartao){
+              System.out.println("Pede um ifood e um Ze delivery pae");
+          } else if (temDinheiro || temCartao) {
+              System.out.println("Pede so um ifood pae");
+          }
+          else {
+              System.out.println("Tem que economizar papae ");
+          }
+    }
+}
