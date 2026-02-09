fe8334230f6630e0e58afeeb33dc8fc730237fab
Lucas <superlucas2005@gmail.com>
2026-02-09 17:35:55 -0300

    Swtich - Case

---
+++ primeiro_projeto/src/SwitchCase.java
@@ -0,0 +1,29 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        int diaSemana = 1;
+
+        switch (diaSemana) {
+            case 1:
+                System.out.println("domingo");
+                break;
+            case 2:
+                System.out.println("segunda-feira");
+                break;
+            case 3:
+                System.out.println("terca-feira");
+                break;
+            case 4:
+                System.out.println("quarta-feira");
+                break;
+            case 5:
+                System.out.println("quinta-feira");
+                break;
+            case 6:
+                System.out.println("sexta-feira");
+                break;
+            case 7:
+                System.out.println("sabado-feira");
+                break;
+        }
+    }
+}
