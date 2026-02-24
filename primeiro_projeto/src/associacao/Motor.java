87fc7657c623089cdab54c7724027db393bdcc92
Lucas <superlucas2005@gmail.com>
2026-02-24 10:40:23 -0300

    Pilares do POO: Associacao

---
+++ primeiro_projeto/src/associacao/Motor.java
@@ -0,0 +1,18 @@
+package associacao;
+
+public class Motor {
+
+    private int cilidradas;
+
+    public int getCilidradas() {
+        return cilidradas;
+    }
+
+    public void setCilidradas(int cilidradas) {
+        this.cilidradas = cilidradas;
+    }
+
+    public Motor(int cilidradas) {
+        this.cilidradas = cilidradas;
+    }
+}
---
+++ primeiro_projeto/src/associacao/Principal.java
@@ -0,0 +1,9 @@
+package associacao;
+
+public class Principal {
+    public static void main(String[] args) {
+        Carro fusca = new Carro(20);
+        fusca.mostrarCilindradas();
+
+    }
+}
